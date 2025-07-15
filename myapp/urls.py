b476856a8a4596b3092f23047717067408a0b449
u65042380101 <pqjbsqqq@gmail.com>
2025-07-16 00:14:38 +0700

    first commit.

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,8 @@ from myapp import views
 
 urlpatterns = [
     path('',views.index),
-    path('about', views.about),
-    path('form',views.form),
+    path('about/', views.about),
+    path('form/',views.form),
     path('edit/<person_id>/', views.edit),
     path('delete/<person_id>/', views.delete)
 ]   
